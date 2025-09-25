4a850b5120431cb5dc29dcdeecd0d38fb66b2bda
RuanzinF <solerzera2007@gmail.com>
2025-09-25 12:06:02 -0300

    yu

--- sic2025/settings.py
+++ sic2025/settings.py
@@ -136,11 +136,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-
-STATICFILES_DIRS = [ 
-    BASE_DIR / "static", #Configura 
-
+STATIC_URL = 'static/'
+STATIC_ROOT = 'static_gcloud/'  # Alterar essa configuração
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
 ]
 
 # Default primary key field type
